c33383515ebb429ec7e041cda26f2e4c96bd12c1
alejandro <aortega@prosolvitech.com>
2023-02-02 08:10:04 -0600

    cors

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from routes.beneficiary import beneficiary as beneficiaryRoute
 from database.manager import engine
 import models.employeeModel
 import models.beneficiaryModel
+from fastapi.middleware.cors import CORSMiddleware
 
 models.employeeModel.Base.metadata.create_all(bind=engine)
 models.beneficiaryModel.Base.metadata.create_all(bind=engine)
@@ -13,6 +14,14 @@ main = FastAPI(
     title="API KSP backend",
     description="Api from employee and beneficiary",
     version="1.0"
+)
+
+main.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 main.include_router(employeeRoute.router, tags=["Employee"])
